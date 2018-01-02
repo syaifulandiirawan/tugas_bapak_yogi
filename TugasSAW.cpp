1dbc63336607d2a8967b23cf0cace10119384363
syaifulandiirawan <32381985+syaifulandiirawan@users.noreply.github.com>
2018-01-02 01:23:50 -0800

    Menambahkan Variabel mc dan isi nilainya
    Menambahkan Variabel mc dan isi nilainya

--- TugasSAW.cpp
+++ TugasSAW.cpp
@@ -19,4 +19,6 @@ int main(){
 	double Ra1c[5]={ (a1c[0]/mc[0]),(a1c[1]/mc[1]),(a1c[2]/mc[2]),(a1c[3]/mc[3]),(a1c[4]/mc[4])};
 	double Ra2c[5]={ (a2c[0]/mc[0]),(a2c[1]/mc[1]),(a2c[2]/mc[2]),(a2c[3]/mc[3]),(a2c[4]/mc[4])};
 	double Ra3c[5]={ (a3c[0]/mc[0]),(a3c[1]/mc[1]),(a3c[2]/mc[2]),(a3c[3]/mc[3]),(a3c[4]/mc[4])};
+	
+	double w[5]={5,3,4,4,2};
 }
